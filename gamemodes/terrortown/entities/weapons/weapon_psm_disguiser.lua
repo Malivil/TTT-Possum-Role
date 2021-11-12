4e242deaf0f9e0bf3e4af7fbfd7f0424d5bdeb81
Malivil <mlebson@outlook.com>
2021-11-12 17:35:41 -0500

    Completed UI work

--- gamemodes/terrortown/entities/weapons/weapon_psm_disguiser.lua
+++ gamemodes/terrortown/entities/weapons/weapon_psm_disguiser.lua
@@ -23,20 +23,35 @@ SWEP.UseHands               = true
 SWEP.LimitedStock           = true
 SWEP.AmmoEnt                = nil
 
-SWEP.Primary.Delay          = 10
+SWEP.Primary.Delay          = 1
 SWEP.Primary.Automatic      = false
 SWEP.Primary.Cone           = 0
 SWEP.Primary.Ammo           = nil
-SWEP.Primary.ClipSize       = -1
-SWEP.Primary.ClipMax        = -1
-SWEP.Primary.DefaultClip    = -1
+SWEP.Primary.ClipSize       = 100
+SWEP.Primary.ClipMax        = 100
+SWEP.Primary.DefaultClip    = 100
 SWEP.Primary.Sound          = ""
 
+if SERVER then
+    CreateConVar("ttt_possum_disguiser_drain", "0.32", FCVAR_NONE, "The drain delay", 0.01, 1)
+    CreateConVar("ttt_possum_disguiser_recharge", "0.16", FCVAR_NONE, "The recharge delay", 0.01, 1)
+end
+
 function SWEP:Initialize()
+    self.lastTickSecond = 0
     self:SendWeaponAnim(ACT_SLAM_DETONATOR_DRAW)
+
+    if CLIENT then
+        self:AddHUDHelp("psm_disguiser_help_pri", "psm_disguiser_help_sec", true)
+    end
+    return self.BaseClass.Initialize(self)
 end
 
 function SWEP:Equip()
+end
+
+function SWEP:Holster()
+    return not self:GetOwner():GetNWBool("PossumDisguiseRunning", false)
 end
 
 function SWEP:Deploy()
@@ -45,4 +60,38 @@ function SWEP:Deploy()
 end
 
 function SWEP:PrimaryAttack()
+    local owner = self:GetOwner()
+    if owner:GetNWBool("PossumDisguiseRunning", false) then return end
+
+    self:SendWeaponAnim(ACT_SLAM_DETONATOR_DETONATE)
+
+    -- Toggle state
+    owner:SetNWBool("PossumDisguiseActive", not owner:GetNWBool("PossumDisguiseActive", false))
+end
+
+function SWEP:Think()
+    if CLIENT then return end
+
+    local owner = self:GetOwner()
+    local running = owner:GetNWBool("PossumDisguiseRunning", false)
+    local rate = running and GetConVar("ttt_possum_disguiser_drain"):GetFloat() or GetConVar("ttt_possum_disguiser_recharge"):GetFloat()
+
+    if CurTime() - self.lastTickSecond > rate then
+        local clip = self:Clip1()
+        -- If they run out of charge, disable the disguiser
+        if running and clip == 0 then
+            owner:SetNWBool("PossumDisguiseRunning", false)
+        else
+            if running then
+                clip = clip - 1
+            else
+                clip = clip + 1
+            end
+
+            if clip < 0 or clip > self:GetMaxClip1() then return end
+
+            self:SetClip1(clip)
+            self.lastTickSecond = CurTime()
+        end
+    end
 end
--- lua/customroles/possum.lua
+++ lua/customroles/possum.lua
@@ -20,14 +20,104 @@ ROLE.loadout = {"weapon_psm_disguiser"}
 
 ROLE.translations = {
     ["english"] = {
-        ["psm_disguiser"] = "Death Disguiser"
+        ["psm_disguiser"] = "Death Disguiser",
+        ["psm_disguiser_charge"] = "DISGUISE REMAINING",
+        ["psm_disguiser_hud"] = "Death disguiser active",
+        ["psm_disguiser_help_pri"] = "Use {primaryfire} to toggle the device on and off",
+        ["psm_disguiser_help_sec"] = "While the device is active, taking damage will cause you to play dead"
     }
 }
 
 ROLE.convars = {}
+table.insert(ROLE.convars, {
+    cvar = "ttt_possum_disguiser_drain",
+    type = ROLE_CONVAR_TYPE_NUM,
+    decimal = 2
+})
+table.insert(ROLE.convars, {
+    cvar = "ttt_possum_disguiser_recharge",
+    type = ROLE_CONVAR_TYPE_NUM,
+    decimal = 2
+})
 
 RegisterRole(ROLE)
 
 if SERVER then
     AddCSLuaFile()
+
+    hook.Add("EntityTakeDamage", "Possum_EntityTakeDamage", function(ent, dmginfo)
+        if not IsPlayer(ent) or not ent:Alive() or ent:IsSpec() or not ent:IsPossum() then return end
+        if not ent:GetNWBool("PossumDisguiseActive", false) then return end
+
+        local att = dmginfo:GetAttacker()
+        if not IsPlayer(att) or att == ent then return end
+
+        -- Play dead
+        ent:SetNWBool("PossumDisguiseRunning", true)
+        ent:SelectWeapon("weapon_psm_disguiser")
+        -- TODO: Ragdoll, lock view
+    end)
+
+    hook.Add("TTTPrepareRound", "Possum_PrepareRound", function()
+        for _, v in pairs(player.GetAll()) do
+            v:SetNWBool("PossumDisguiseActive", false)
+            v:SetNWBool("PossumDisguiseRunning", false)
+        end
+    end)
+
+    hook.Add("TTTPlayerRoleChanged", "Possum_TTTPlayerRoleChanged", function(ply, oldRole, newRole)
+        ply:SetNWBool("PossumDisguiseActive", false)
+        ply:SetNWBool("PossumDisguiseRunning", false)
+    end)
+end
+
+if CLIENT then
+    surface.CreateFont("PSMTimeLeft", {
+        font = "Trebuchet24",
+        size = 22,
+        weight = 600
+    })
+
+    hook.Add("TTTHUDInfoPaint", "Possum_TTTHUDInfoPaint", function(client, label_left, label_top)
+        if not IsPlayer(client) or not client:Alive() or client:IsSpec() or not client:IsPossum() then return end
+        if not client:GetNWBool("PossumDisguiseActive", false) then return end
+
+        surface.SetFont("TabLarge")
+        surface.SetTextColor(255, 255, 255, 230)
+
+        text = LANG.GetTranslation("psm_disguiser_hud")
+        local _, h = surface.GetTextSize(text)
+
+        surface.SetTextPos(label_left, ScrH() - label_top - h)
+        surface.DrawText(text)
+
+        -- Move the label up for the next one
+        label_top = label_top + 20
+    end)
+
+    -- Disguise time progress bar
+    local margin = 10
+    local width, height = 250, 25
+    local x = ScrW() / 2 - width / 2
+    local y = margin / 2 + height
+    local colors = {
+        background = Color(30, 60, 100, 222),
+        fill = Color(75, 150, 255, 255)
+    }
+    hook.Add("HUDPaint", "Possum_HUDPaint", function()
+        local client = LocalPlayer()
+        if not IsPlayer(client) then return end
+
+        local weap = client:GetActiveWeapon()
+        if not IsValid(weap) then return end
+
+        if WEPS.GetClass(weap) ~= "weapon_psm_disguiser" then return end
+
+        local max = weap:GetMaxClip1()
+        local diff = max - weap:Clip1()
+        if diff > 0 then
+            HUD:PaintBar(8, x, y, width, height, colors, 1 - (diff / max))
+            draw.SimpleText(LANG.GetTranslation("psm_disguiser_charge"), "PSMTimeLeft", ScrW() / 2, y + 1, COLOR_WHITE, TEXT_ALIGN_CENTER)
+        end
+    end)
 end
